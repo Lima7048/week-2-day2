fcbc18447db62256ddda619da834ae245e5f35fa
salima <brycesalima@gmail.com>
2024-01-16 11:45:03 +0000

    learning object and complex object

---
+++ app.js
@@ -0,0 +1,33 @@
+console.log("Hello world!")
+
+const human = {
+    name: "Bob",// Output: Bob
+    age: 30,// Output: 30
+    favouriteColour: "red",// Output: red
+};
+console.log(human);
+
+console.table(human);
+
+const person = {
+    name: "Bob",
+    age: 30,
+    favouriteColour: "red",
+};
+
+person.favouriteFood = "pizza";
+
+console.log(person.favouriteFood);// Output: pizza
+
+
+const car = { make: "Toyota", model: 1996, color: "Metallic Pink", }
+console.log(car)
+
+const book = {
+    title: "Princess Diaries",
+    author: "Meg Cabot",
+    numberofpages: "367pg",
+}
+console.log(book)
+book.genre = "Young Adult Fiction"
+console.log(book.genre)
